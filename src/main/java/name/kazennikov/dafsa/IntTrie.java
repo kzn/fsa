fbcfde67e3639a82042fbe862896f78e0b18ffcd
kazennikov <kazennikov@gmail.com>
2012-07-02 13:16:45 +0400

    added javadocs

--- src/main/java/name/kazennikov/dafsa/IntTrie.java
+++ src/main/java/name/kazennikov/dafsa/IntTrie.java
@@ -7,6 +7,12 @@ import gnu.trove.map.hash.TLongIntHashMap;
 import gnu.trove.set.TIntSet;
 import gnu.trove.set.hash.TIntHashSet;
 
+/**
+ * Hash-based trie for int-length labels. Uses smaller memory footprint than {@link CharFSA}
+ * when constructing trie-based structures
+ * @author Anton Kazennikov
+ *
+ */
 public class IntTrie {
 	int states = 1;
 	TLongIntHashMap trans = new TLongIntHashMap();
@@ -15,11 +21,19 @@ public class IntTrie {
 	public int size() {
 		return states;
 	}
-	
+	/**
+	 * Get final features of a state
+	 * @return null, if state doesn't have final features
+	 */
 	public TIntSet getFinals(int state) {
 		return finals.get(state);
 	}
 	
+	/**
+	 * Add a final feature to the state
+	 * @param state state number
+	 * @param fin final feature
+	 */
 	public void addFinal(int state, int fin) {
 		TIntSet stateFinals = finals.get(state);
 		
@@ -38,14 +52,34 @@ public class IntTrie {
 		return key;
 	}
 	
+	/**
+	 * Add transition deterministically to the trie
+	 * @param srcState source state number
+	 * @param label transition label
+	 * @param nextState destination state number
+	 */
 	public void setNext(int srcState, int label, int nextState) {
 		trans.put(getKey(srcState, label), nextState);
 	}
 	
+	/**
+	 * Get next state on current state and transition label
+	 * @param srcState source state number
+	 * @param label transition label
+	 * @return state number, or 0, if there is no such transition
+	 */
 	public int getNext(int srcState, int label) {
 		return trans.get(getKey(srcState, label));
 	}
 	
+	/**
+	 * Add suffix to the given state
+	 * @param state source state
+	 * @param seq sequence, containing the suffix
+	 * @param startOffset suffix start offset
+	 * @param fin final feature of the suffix
+	 * @return end state number
+	 */
 	public int addSuffix(int state, TIntArrayList seq, int startOffset, int fin) {
 		for(int i =  startOffset; i < seq.size(); i++) {
 			int nextState = ++states;
@@ -57,6 +91,11 @@ public class IntTrie {
 		return states;
 	}
 	
+	/**
+	 * Add given sequence with given final feature to the trie
+	 * @param seq sequence
+	 * @param finals final feature
+	 */
 	public void add(TIntArrayList seq, int finals) {
 		if(seq.isEmpty())
 			return; 
@@ -81,6 +120,11 @@ public class IntTrie {
 	}
 	
 	
+	/**
+	 * Add given sequence (through iterator) with given final feature to the trie
+	 * @param it sequence iterator
+	 * @param finals final feature
+	 */
 	public void add(TIntIterator it, int finals) {
 		if(!it.hasNext())
 			return;
