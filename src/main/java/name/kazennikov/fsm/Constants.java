4c15577b71f71da44958d451e8bb62f3823a09f4
Anton Kazennikov <kazennikov@gmail.com>
2013-08-10 13:56:47 +0400

    basic fsm implementation

---
+++ src/main/java/name/kazennikov/fsm/Constants.java
@@ -0,0 +1,18 @@
+/*
+ *  Constants.java
+ *
+ *  Copyright (c) 2010-2011, Ontotext (www.ontotext.com).
+ *
+ *  This file is part of GATE (see http://gate.ac.uk/), and is free
+ *  software, licenced under the GNU Library General Public License,
+ *  Version 2, June 1991 (in the distribution as file licence.html,
+ *  and also available at http://gate.ac.uk/gate/licence.html).
+ *
+ *
+ *  $Id$
+ */
+package name.kazennikov.fsm;
+
+public class Constants {
+	public static final int NO = -1;
+}
