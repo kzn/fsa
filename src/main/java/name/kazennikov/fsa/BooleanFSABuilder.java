3b703c1fa8ea0606fba80d4abc13cbda6505c40a
Anton Kazennikov <kazennikov@gmail.com>
2015-01-28 22:02:36 +0300

    fsa builder helper class

---
+++ src/main/java/name/kazennikov/fsa/BooleanFSABuilder.java
@@ -0,0 +1,34 @@
+package name.kazennikov.fsa;
+
+import gnu.trove.list.TIntList;
+import name.kazennikov.dafsa.IntDAFSABoolean;
+import name.kazennikov.fsa.walk.WalkFSABoolean;
+
+/**
+ * Simple FSA builder helper class
+ */
+public class BooleanFSABuilder {
+    IntDAFSABoolean fsa = new IntDAFSABoolean();
+
+    public BooleanFSABuilder() {
+        fsa.setFinalValue(true);
+    }
+
+    public void add(TIntList seq) {
+        fsa.add(seq);
+    }
+
+    public void addMinWord(TIntList seq) {
+        fsa.addMinWord(seq);
+    }
+
+    public int size() {
+        return fsa.size();
+    }
+
+    public WalkFSABoolean build() {
+        WalkFSABoolean.Builder builder = new WalkFSABoolean.Builder();
+        fsa.emit(builder);
+        return builder.build();
+    }
+}
